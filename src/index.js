6aaa655b5ea60a2a35fe8c6debd9da75aa91bad5
howardshowered <194877133+howardshowered@users.noreply.github.com>
2025-10-02 12:49:27 -0700

    Revert "added all webpack components. Added module to load page. Added template. Added css styling."
    This reverts commit 5ed0fb2882b989a0f2173c99a4992571414532ff.

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { greeting } from "./greeting.js";
-import "./style.css";
-import { loadPage } from "./page.js";
-
-console.log(greeting);
-
-loadPage();
-// const headerElement = document.querySelector("header");
-
